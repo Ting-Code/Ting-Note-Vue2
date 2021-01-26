fdf489d389b86beab3f21716fbf95c55dfcdc602
TINGCYGF <1127163160@qq.com>
2021-01-26 11:06:29 +0800

    添加基本页面与配置路由

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      message: '你好我来了'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
