4cf57e905ada80dc413c1cb1dabb522b483bf05b
TINGCYGF <1127163160@qq.com>
2021-04-26 09:26:08 +0800

    部署

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+npm run build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "Update" &&
+git branch -M master &&
+git remote add origin git@gitee.com:TINGCYGF/note-view.git &&
+git push -f -u origin master &&
+cd - &&
+echo https://tingcygf.gitee.io/ting-ui-vue3/#/
