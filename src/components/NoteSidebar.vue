fb35c0c0a134f9c358f07c053ad16e0d76f59280
TINGCYGF <1127163160@qq.com>
2021-06-17 17:37:43 +0800

    调整样式

--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -44,7 +44,7 @@ export default {
   created() {
     this.getNotebooks().then(() => {
       this.setCurBook({curBookId: this.$route.query.notebookId})
-      //额('setCurBook',{curBookId: this.$route.query.notebookId})
+      //('setCurBook',{curBookId: this.$route.query.notebookId})
       return this.getNotes({notebookId: this.curBook.id})
     }).then(() => {
       this.setCurNote({curNoteId: this.$route.query.noteId})
